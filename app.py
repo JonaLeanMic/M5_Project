f1f8777e5db447f95aac918b8927a529c438f7e9
jonat <jonathan.michel@student.fh-kiel.de>
2024-01-13 00:44:17 +0100

    Changes by miria

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import flask
 from measurementManager import MeasurementManager
 import RPi.GPIO as GPIO
 from flask_socketio import SocketIO
@@ -15,15 +14,18 @@ socketio = SocketIO(app)
 mm = MeasurementManager(Magnet_Pin, socketio)
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html', data=mm.data, measuring=GPIO.event_detected(Interrupt_Pin))
+
 
 @app.route('/start_measurement', methods=['POST'])
 def start_measurement():
     if request.method == 'POST':
         GPIO.add_event_detect(Interrupt_Pin, GPIO.FALLING, callback=mm.interrupt, bouncetime=250)
     return render_template('index.html', data=mm.data, measuring=GPIO.event_detected(Interrupt_Pin))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
