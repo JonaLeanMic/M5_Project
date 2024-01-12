7fe5bdda3f4ed09e20598bebc8b36e990a649275
jonat <jonathan.michel@student.fh-kiel.de>
2024-01-12 22:19:38 +0100

    -changed to working in 1 file

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import flask
+from measurementManager import MeasurementManager
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+Interrupt_Pin = 18
+GPIO.setup(Interrupt_Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+mm = MeasurementManager()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=mm.data, measuring=GPIO.event_detected(Interrupt_Pin))
+
+@app.route('/start_measurement', methods=['POST'])
+def start_measurement():
+    if request.method == 'POST':
+        GPIO.add_event_detect(Interrupt_Pin, GPIO.FALLING, callback=mm.interrupt, bouncetime=250)
+    return render_template('index.html', data=mm.data, measuring=GPIO.event_detected(Interrupt_Pin))
+
+if __name__ == '__main__':
+    app.run(debug=True)
