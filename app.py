6f8456f043972822f1d9de915e7f267274fde934
Karl Hungus <vhkchvuoudgcwwqkxk@miucce.online>
2024-01-13 14:57:48 +0100

    EInbau von API - Singleton MeasurementManager (MM)- Anpassungen MM - - einbau Javascript für state und Daten

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from measurementManager import MeasurementManager
 import RPi.GPIO as GPIO
+import json
 
 
 GPIO.setmode(GPIO.BCM)
@@ -9,22 +10,43 @@ Magnet_Pin = 23  # achtung nur beispiel zahl, überprüfen
 
 app = Flask(__name__)
 
-mm = MeasurementManager(Magnet_Pin)
-
-
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=mm.data)
+    return render_template('index.html')
 
 
 @app.route('/start_measurement', methods=['POST'])
 def start_measurement():
     if request.method == 'POST':
+        mm = MeasurementManager.instance()
         #GPIO.output(Magnet_Pin, GPIO.LOW) #magnet ausschalten
-        mm.start = True
+        mm.startMeasurement()
         #hier könnte man auch eine variable start im mm auf true setzen
-    return render_template('index.html', data=mm.data)
+    return redirect("/")
+
+
+#api-route für daten
+@app.route('/getData', methods=['POST'])
+def getJsonDate():
+    if request.method == 'POST':
+        mm = MeasurementManager.instance()
+
+    return json.dumps(mm.getData())
+
+#api route für systemzustand
+@app.route('/getMeasureState')
+def getMeasureState():
+    mm = MeasurementManager.instance()
+    return mm.getMeasurementStatus()
+
+#api-route um messungen abzubrechen (usability)
+@app.route('/abort_Measurement')
+def abortMeasurement():
+    mm = MeasurementManager.instance()
+    mm.abortMeasurement()
+    return redirect("/")
+
 
 
 if __name__ == '__main__':
--- measurementManager.py
+++ measurementManager.py
@@ -1,51 +1,98 @@
 import time
 from RPi import GPIO
 
-GPIO.setmode(GPIO.BCM)
-Interrupt_Pin = 18
-GPIO.setup(Interrupt_Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-class MeasurementManager:
+#singleton 
+#siehe https://python-patterns.guide/gang-of-four/singleton/
+class MeasurementManager(object):
+    _instance = None
 
-    def __init__(self, magnet_pin):
-        self.start = False
-        self.timeInterrupt = 0
-        self.timePeriodSwing = 0
-        self.timeSwingStart = 0
+    def __init__(self):
+        raise RuntimeError('Nicht erlaubt - rufe instance() um einen Manager zu erhalten ')
 
-        self.maxSwings = 10
-        self.interruptCount = 0
-        self.swingCount = 0
-        self.start = False
+
+
+    @classmethod
+    def instance(self):
+        if self._instance is None:
+            print('Erstelle neue Manager Instanz')
+            self._instance = self.__new__(self)
+            self.start = False
+            self.timeInterrupt = 0
+            self.timePeriodSwing = 0
+            self.timeSwingStart = 0
+            self.maxSwings = 10
+            self.interruptCount = 0
+            self.swingCount = 0
+            self.start = False
+            self.data = []
+            GPIO.setmode(GPIO.BCM)
+            self.magnet_pin = 23
+            Interrupt_Pin = 18
+            GPIO.setup(Interrupt_Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(Interrupt_Pin, GPIO.FALLING, callback=self.interrupt, bouncetime=250)
+
+        return self._instance
+
+    #gibt daten zurück
+    def getData(self):
+        return self.data
+
+    #setzt start auf wahr
+    #reinigt list 
+    #vielleicht speichern wir die Listen in einer Datei?
+    def startMeasurement(self):
+        print("starting measurement")
+        self.start = True
+        self.setMagnetState(False)
         self.data = []
-        self.magnet_pin = magnet_pin
 
-        GPIO.add_event_detect(Interrupt_Pin, GPIO.FALLING, callback=self.interrupt, bouncetime=250)
+    #beendet die messung ohne die Liste zu reinigen 
+    def endMeasurement(self):
+        print("ending measurement")
+        self.start = False
+        self.setMagnetState(False)
 
-    #def is_magnet_on(self):
-        #return GPIO.input(self.magnet_pin) == GPIO.HIGH
+    #beendet messung, schaltet magnet an, reinigt liste (um neuen Durchgang zu starten wenn etwas schiefgeht)
+    def abortMeasurement(self):
+        print("aborting measurement")
+        self.start = False
+        self.setMagnetState(True)
+        self.data = []
+        
+
+    #gibt messzustand aus 
+    def getMeasurementStatus(self):
+        return self.start
+
+    #ändert Magnet Zustand 
+    def setMagnetState(self, state):
+        if state:
+            GPIO.output(GPIO.HIGH)
+        else:
+            GPIO.output(GPIO.LOW)
 
     # function to stop the time of the last full swing
-    def interrupt(self, channel):
-        #if not self.is_magnet_on():
-        if self.start:
-            if self.swingCount <= self.maxSwings:
+    def interrupt(self):
+        #wenn messung läuft und noch nicht alle werte gesammelt wurden
+        if self.swingCount < self.maxSwings and self.start:
+                    # the first full swings starts after the first interrupt
+                    if self.interruptCount == 0:
+                        print("start")
+                        self.timeInterrupt = time.monotonic()
+                    # every second interrupt is a full swing
+                    elif self.interruptCount % 2 == 0:
+                        print(self.swingCount)
+                        self.timeSwingStart = self.timeInterrupt
+                        self.timeInterrupt = time.monotonic()
+                        self.timePeriodSwing = self.timeInterrupt - self.timeSwingStart
+                        print(str(self.timePeriodSwing))
 
-                # the first full swings starts after the first interrupt
-                if self.interruptCount == 0:
-                    print("start")
-                    self.timeInterrupt = time.monotonic()
-                # every second interrupt is a full swing
-                elif self.interruptCount % 2 == 0:
-                    print(self.swingCount)
-                    self.timeSwingStart = self.timeInterrupt
-                    self.timeInterrupt = time.monotonic()
-                    self.timePeriodSwing = self.timeInterrupt - self.timeSwingStart
-                    print(str(self.timePeriodSwing))
-
-                    self.data.append(self.timePeriodSwing)
+                        self.data.append(self.timePeriodSwing)
 
 
-                self.interruptCount += 1
-                self.swingCount = self.interruptCount/2
+                    self.interruptCount += 1
+                    self.swingCount = self.interruptCount/2
+        else:
+            self.endMeasurement()
