9cd19e01144a350428c19e57f5680b27bb4bd30e
Karl Hungus <vhkchvuoudgcwwqkxk@miucce.online>
2024-01-13 16:08:12 +0100

    Im video gezeigte version - fake measurement manager - getestet

---
+++ MOCKUPmeasurementManager.py
@@ -0,0 +1,77 @@
+
+from time import sleep
+from random import randrange
+from threading import Thread
+
+#singleton 
+#siehe https://python-patterns.guide/gang-of-four/singleton/
+
+
+
+class MeasurementManager(object):
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Nicht erlaubt - rufe instance() um einen Manager zu erhalten ')
+
+
+
+    @classmethod
+    def instance(self):
+        if self._instance is None:
+            print('Erstelle neue Manager Instanz')
+            self._instance = self.__new__(self)
+            self.start = False
+            self.data = []
+
+        return self._instance
+
+    #gibt daten zurück
+    def getData(self):
+        return self.data
+
+    #setzt start auf wahr
+    #reinigt list 
+    #vielleicht speichern wir die Listen in einer Datei?
+    def startMeasurement(self):
+        if self.start == False:
+            print("[MOCKUP] starting measurement")
+            self.start = True
+            self.setMagnetState(False)
+            self.data = []
+            self.fakedatathread = Thread(target=generateFakeMeasurements)
+            self.fakedatathread.start()
+        else:
+            print("Measurement is in progress")
+
+    #beendet die messung ohne die Liste zu reinigen 
+    def endMeasurement(self):
+        print("[MOCKUP] ending measurement")
+        self.start = False
+        self.setMagnetState(False)
+        self.fakedatathread.join()
+
+    #beendet messung, schaltet magnet an, reinigt liste (um neuen Durchgang zu starten wenn etwas schiefgeht)
+    def abortMeasurement(self):
+        print("aborting measurement")
+        self.start = False
+        self.setMagnetState(True)
+        self.data = []
+        self.fakedatathread.join()
+
+    #gibt messzustand aus 
+    def getMeasurementStatus(self):
+        return self.start
+
+    #ändert Magnet Zustand 
+    def setMagnetState(self, state):
+        print("Magnetstatus: " , state)
+
+def generateFakeMeasurements():
+    mm = MeasurementManager.instance()
+    counter = 0
+    while counter < 10:
+        mm.data.append( randrange(0,100))
+        counter = counter + 1 
+        sleep(1)
+    mm.endMeasurement()
--- app.py
+++ app.py
@@ -1,53 +1,58 @@
 from flask import Flask, render_template, request, redirect
-from measurementManager import MeasurementManager
-import RPi.GPIO as GPIO
+from MOCKUPmeasurementManager import MeasurementManager
 import json
 
 
-GPIO.setmode(GPIO.BCM)
-Magnet_Pin = 23  # achtung nur beispiel zahl, überprüfen
-#PIO.setup(Magnet_Pin, GPIO.OUT, initial=GPIO.HIGH) #magnet zu beginn eingeschaltet, Zeile nicht geprüft
+from flask_classful import FlaskView, route
+import random
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class TestView(FlaskView):
+    @route('/')
+    def index(self):
+        return render_template('index.html')
 
 
-@app.route('/start_measurement', methods=['POST'])
-def start_measurement():
-    if request.method == 'POST':
+    @route('/start_measurement')
+    def start_measurement(self):
+
         mm = MeasurementManager.instance()
         #GPIO.output(Magnet_Pin, GPIO.LOW) #magnet ausschalten
         mm.startMeasurement()
         #hier könnte man auch eine variable start im mm auf true setzen
-    return redirect("/")
+        return redirect("/")
 
 
-#api-route für daten
-@app.route('/getData', methods=['POST'])
-def getJsonDate():
-    if request.method == 'POST':
+    #api-route für daten
+    @route('/getData')
+    def getJsonData(self):
+
         mm = MeasurementManager.instance()
 
-    return json.dumps(mm.getData())
+        return json.dumps(mm.getData())
 
-#api route für systemzustand
-@app.route('/getMeasureState')
-def getMeasureState():
-    mm = MeasurementManager.instance()
-    return mm.getMeasurementStatus()
+    #api route für systemzustand
+    @route('/getMeasureState')
+    def getMeasureState(self):
+        mm = MeasurementManager.instance()
+        return str(mm.getMeasurementStatus())
 
-#api-route um messungen abzubrechen (usability)
-@app.route('/abort_Measurement')
-def abortMeasurement():
-    mm = MeasurementManager.instance()
-    mm.abortMeasurement()
-    return redirect("/")
+    #api-route um messungen abzubrechen (usability)
+    @route('/abort_Measurement')
+    def abortMeasurement(self):
+        mm = MeasurementManager.instance()
+        mm.abortMeasurement()
+        return redirect("/")
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+TestView.register(app, route_base='/')
+
+app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+
+while 1:
+    None
