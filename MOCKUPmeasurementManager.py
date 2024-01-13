ebb97a33cfc6d278b4528563aec5162cb8ca5fbc
Ash <miriam.bernhardt@student.fh-kiel.de>
2024-01-13 23:33:52 +0100

    zusammengeführter Kram

--- MOCKUPmeasurementManager.py
+++ MOCKUPmeasurementManager.py
@@ -3,46 +3,37 @@ from random import randrange
 from threading import Thread
 from FileCreator import FileCreator
 
-#singleton 
-#siehe https://python-patterns.guide/gang-of-four/singleton/
 
-class MeasurementManager(object):
-    _instance = None
+# singleton
+# siehe https://python-patterns.guide/gang-of-four/singleton/
 
-    def __init__(self):
-        raise RuntimeError('Nicht erlaubt - rufe instance() um einen Manager zu erhalten ')
+class MeasurementManager:
 
-    @classmethod
-    def instance(self):
-        if self._instance is None:
-            print("Erstelle neue Manager Instanz")
-            self._instance = self.__new__(self)
-            self.start = False
-            self.data = []
+    def _init_(self):
+        self.start = False
+        self.data = []
 
-            self.lastFilePath = ""
+        self.lastFilePath = ""
 
-        return self._instance
-
-    #gibt die momentanen messdaten daten zur�ck
+    # gibt die momentanen messdaten daten zur�ck
     def getData(self):
         return self.data
 
-    #setzt start auf wahr
-    #reinigt list 
-    #vielleicht speichern wir die Listen in einer Datei?
+    # setzt start auf wahr
+    # reinigt list
+    # vielleicht speichern wir die Listen in einer Datei?
     def startMeasurement(self):
         if self.start == False:
             print("[MOCKUP] starting measurement")
             self.start = True
             self.setMagnetState(False)
             self.data = []
-            self.fakedatathread = Thread(target=generateFakeMeasurements)
+            self.fakedatathread = Thread(target=self.generateFakeMeasurements)
             self.fakedatathread.start()
         else:
             print("Measurement is in progress")
 
-    #beendet die messung ohne die Liste zu reinigen 
+    # beendet die messung ohne die Liste zu reinigen
     def endMeasurement(self):
         if self.start == True:
             print("[MOCKUP] ending measurement")
@@ -54,8 +45,7 @@ class MeasurementManager(object):
             except Exception as e:
                 print("Error in End Measurment, the thread has probably already been ended ")
 
-
-    #beendet messung, schaltet magnet an, reinigt liste (um neuen Durchgang zu starten wenn etwas schiefgeht)
+    # beendet messung, schaltet magnet an, reinigt liste (um neuen Durchgang zu starten wenn etwas schiefgeht)
     def abortMeasurement(self):
         if self.start == True:
             print("aborting measurement")
@@ -68,21 +58,18 @@ class MeasurementManager(object):
                 print("Error in Abort Measurment, the thread has probably already been ended ")
                 print(e)
 
-    #gibt messzustand aus 
+    # gibt messzustand aus
     def getMeasurementStatus(self):
         return self.start
 
-    #�ndert Magnet Zustand 
+    # �ndert Magnet Zustand
     def setMagnetState(self, state):
-        print("Magnetstatus: " , state)
+        print("Magnetstatus: ", state)
 
-
-
-def generateFakeMeasurements():
-    mm = MeasurementManager.instance()
-    counter = 0
-    while counter < 10:
-        mm.data.append( randrange(0,100))
-        counter = counter + 1 
-        sleep(0.2)
-    mm.endMeasurement()
+    def generateFakeMeasurements(self):
+        counter = 0
+        while counter < 10:
+            self.data.append(randrange(0, 100))
+            counter = counter + 1
+            sleep(0.2)
+        self.endMeasurement()
